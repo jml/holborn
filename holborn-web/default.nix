58c5e016ffe3487045a770cb7bf7a5bde8c662fb
Jonathan Lange <jml@mumak.net>
2015-07-24 15:41:17 +0100

    Render example code

--- holborn-web/default.nix
+++ holborn-web/default.nix
@@ -1,11 +1,15 @@
-{ mkDerivation, base, basic-prelude, highlighter2, Spock, stdenv }:
+{ mkDerivation, base, basic-prelude, blaze-html, highlighter2
+, Spock, stdenv
+}:
 mkDerivation {
   pname = "holborn-web";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  buildDepends = [ base basic-prelude highlighter2 Spock ];
+  buildDepends = [
+    base basic-prelude blaze-html highlighter2 Spock
+  ];
   homepage = "https://github.com/jml/holborn";
   description = "Code search";
   license = stdenv.lib.licenses.gpl3;
--- holborn-web/src/Main.hs
+++ holborn-web/src/Main.hs
@@ -2,15 +2,43 @@ module Main where
 
 import BasicPrelude
 
+import qualified Data.List as List
+import Text.Blaze.Html (Html)
+import Text.Blaze.Html.Renderer.Utf8 (renderHtml)
+import Text.Highlighter.Formatters.Html (format)
+import Text.Highlighter.Lexer (runLexer)
+import Text.Highlighter.Lexers (lexers)
 import Web.Spock.Safe
 
 import ExampleData (examplePython)
+
+
+-- XXX: When the pattern matching here fails, we get
+-- src/Main.hs:21:5-52: Irrefutable pattern failed for pattern (Just pythonLexer)
+-- and an empty response, rather than the expected 500
+
+-- XXX: Actually handle cases where code won't lex or we can't find a lexer
+
+-- XXX: Is Text the right type?
+
+renderPythonCode :: Text -> Html
+renderPythonCode pythonCode =
+  -- True means "include line numbers".
+  format True tokens
+  where
+    (Right tokens) = runLexer pythonLexer (encodeUtf8 pythonCode)
+    (Just pythonLexer) = List.lookup ".py" lexers
+
+
+-- XXX: Is there a better way of sending blaze to the user with Spock?
+blaze :: MonadIO m => Html -> ActionT m a
+blaze = lazyBytes . renderHtml
 
 
 main :: IO ()
 main =
     runSpock 8080 $ spockT id $
     do get root $
-           text examplePython
+           blaze $ renderPythonCode examplePython
        get ("hello" <//> var) $ \name ->
            text ("Hello " <> name <> "!")
