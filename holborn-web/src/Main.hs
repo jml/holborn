8c0456272f7cb5f505968d84393db0f2790819fe
Jonathan Lange <jml@mumak.net>
2015-07-24 15:03:01 +0100

    Serve example Python source file on the web

--- holborn-web/src/Main.hs
+++ holborn-web/src/Main.hs
@@ -2,13 +2,15 @@ module Main where
 
 import BasicPrelude
 
-import Data.Monoid
 import Web.Spock.Safe
+
+import ExampleData (examplePython)
+
 
 main :: IO ()
 main =
     runSpock 8080 $ spockT id $
     do get root $
-           text "Hello World!"
+           text examplePython
        get ("hello" <//> var) $ \name ->
            text ("Hello " <> name <> "!")
