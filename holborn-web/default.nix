83683425cbfac281fbf4362df3763e35c85e8f26
Jonathan Lange <jml@mumak.net>
2015-07-24 14:40:41 +0100

    Add highlighter2 as a dependency

--- holborn-web/default.nix
+++ holborn-web/default.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, basic-prelude, Spock, stdenv }:
+{ mkDerivation, base, basic-prelude, highlighter2, Spock, stdenv }:
 mkDerivation {
   pname = "holborn-web";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  buildDepends = [ base basic-prelude Spock ];
+  buildDepends = [ base basic-prelude highlighter2 Spock ];
   homepage = "https://github.com/jml/holborn";
   description = "Code search";
   license = stdenv.lib.licenses.gpl3;
