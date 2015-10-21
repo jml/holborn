ffd9a794ae4785bf8fffb127f3e0f38291c4608b
Jonathan Lange <jml@mumak.net>
2015-10-21 14:09:46 +0100

    hypothesis is in nix 15.09

--- holborn-people/shell.nix
+++ holborn-people/shell.nix
@@ -1,25 +1,9 @@
 with (import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-15.09.tar.gz) {}).pkgs;
-let
-# XXX: We probably don't need this any more
-  hypothesis = pythonPackages.buildPythonPackage rec {
-    name = "hypothesis-1.10.6";
-    doCheck = false;
-    src = pkgs.fetchurl {
-      url = "https://pypi.python.org/packages/source/h/hypothesis/${name}.tar.gz";
-      md5 = "333f7bd8b4b0311ea6779cb601de254e";
-    };
-    meta = {
-      description = "A Python library for property based testing";
-      homepage = https://github.com/DRMacIver/hypothesis;
-      license = licenses.mpl20;
-    };
-  };
-in
 pkgs.buildPythonPackage {
   name = "holborn-people";
   srcs = ./.;
   propagatedBuildInputs = [
-    hypothesis
+    pythonPackages.hypothesis
     pythonPackages.django
     pythonPackages.sqlite3
   ];
