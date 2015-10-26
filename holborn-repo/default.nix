963264b7dd0de9f9a9c3a00b44db22b1f8e3496f
Tom Hunger <tehunger@gmail.com>
2015-10-26 13:49:16 +0000

    Remove local .gitignores.

--- holborn-repo/default.nix
+++ holborn-repo/default.nix
@@ -1,9 +1,8 @@
 { mkDerivation, aeson, async, base, basic-prelude, blaze-builder
 , bytestring, either, envparse, http-types, mtl, network
 , network-simple, pipes, pipes-aeson, pipes-bytestring
-, pipes-concurrency, pipes-network, pipes-parse, pipes-safe
-, pipes-shell, pipes-zlib, process, process-streaming, servant
-, servant-server, stdenv, text, time, wai, warp
+, pipes-network, pipes-parse, pipes-safe, pipes-shell, pipes-zlib
+, process, servant, servant-server, stdenv, text, time, wai, warp
 }:
 mkDerivation {
   pname = "holborn-repo";
@@ -14,9 +13,8 @@ mkDerivation {
   buildDepends = [
     aeson async base basic-prelude blaze-builder bytestring either
     envparse http-types mtl network network-simple pipes pipes-aeson
-    pipes-bytestring pipes-concurrency pipes-network pipes-parse
-    pipes-safe pipes-shell pipes-zlib process process-streaming servant
-    servant-server text time wai warp
+    pipes-bytestring pipes-network pipes-parse pipes-safe pipes-shell
+    pipes-zlib process servant servant-server text time wai warp
   ];
   license = stdenv.lib.licenses.unfree;
 }
