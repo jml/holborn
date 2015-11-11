844d411b0098958ba1b806be25d14b0478a7ed60
Jonathan Lange <jml@mumak.net>
2015-11-11 00:40:55 +0000

    Print port on holborn-repo startup
    Duplicates from holborn-syntax

--- holborn-repo/src/Main.hs
+++ holborn-repo/src/Main.hs
@@ -7,22 +7,30 @@ module Main where
 import BasicPrelude
 
 import Network.Wai (Application)
-import Network.Wai.Handler.Warp (run)
+import Network.Wai.Handler.Warp (runSettings)
 import Servant (serve)
 import qualified Env
 import Control.Concurrent (forkIO)
 
 import Holborn.Repo.HttpProtocol (repoServer, repoAPI)
-import Holborn.Repo.Config (Config(..))
+import Holborn.Repo.Config (Config(..), warpSettings)
 import Holborn.Repo.RawProtocol (serveRaw)
 
 -- git init --bare /tmp/hello
 app :: Config -> Application
 app config = serve repoAPI (repoServer config)
 
+-- | Load configuration from the environment.
+loadConfig :: IO Config
+loadConfig =
+  Env.parse (Env.header "run a holborn repo server") $
+  Config <$> Env.var (Env.str Env.<=< Env.nonempty) "REPO_ROOT" (Env.help "path to root of ./org/repo bare repositories")
+             -- XXX: "PORT" copied from holborn-syntax Main.
+         <*> Env.var Env.auto "PORT" (Env.def 8080 <> Env.help "Port to listen on")
+
+
 main :: IO ()
 main = do
-    config <- Env.parse (Env.header "run a holborn repo server") $
-        Config <$> Env.var (Env.str Env.<=< Env.nonempty) "REPO_ROOT" (Env.help "path to root of ./org/repo bare repositories")
-    _ <- forkIO (serveRaw config)
-    run 8080 (app config)
+    config <- loadConfig
+    void $ forkIO (serveRaw config)
+    runSettings (warpSettings config) (app config)
