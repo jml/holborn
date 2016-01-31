816d7b02b6d7dccca7eb2a1c996e502524d43ea6
Jonathan Lange <jml@mumak.net>
2016-01-31 13:31:36 +0000

    Remove unused dependencies

--- holborn-syntax/default.nix
+++ holborn-syntax/default.nix
@@ -1,25 +1,21 @@
 { mkDerivation, base, basic-prelude, blaze-html, blaze-markup
-, bytestring, containers, directory, either, envparse, errors
-, filepath, highlighter2, language-python, mtl, pretty-error
-, servant, servant-blaze, servant-server, stdenv, tasty
-, tasty-hunit, tasty-quickcheck, text, wai, warp
+, bytestring, containers, errors, highlighter2, language-python
+, mtl, pretty-error, servant-server, stdenv, tasty, tasty-hunit
+, tasty-quickcheck, text, wai
 }:
 mkDerivation {
   pname = "holborn-syntax";
   version = "0.2.0.0";
   src = ./.;
-  isLibrary = true;
-  isExecutable = true;
   buildDepends = [
     base basic-prelude blaze-html blaze-markup bytestring containers
-    directory either envparse errors filepath highlighter2
-    language-python mtl pretty-error servant servant-blaze
-    servant-server text wai warp
+    errors highlighter2 language-python mtl pretty-error servant-server
+    text wai
   ];
   testDepends = [
     base basic-prelude tasty tasty-hunit tasty-quickcheck
   ];
   homepage = "https://bitbucket.com/mumak/holborn";
-  description = "Code search";
+  description = "Syntax analysis library";
   license = stdenv.lib.licenses.unfree;
 }
