ba55485c12a7f3774e13834732d3c2bed397e0cf
Tom Hunger <tehunger@gmail.com>
2016-02-22 01:05:51 +0000

    Use Assert.equal in tests.

--- holborn-ui/test/Main.purs
+++ holborn-ui/test/Main.purs
@@ -100,31 +100,31 @@ newtype A = A { name :: String }
 instance eqA :: Eq A where
   eq (A a) (A b) = eq a.name b.name
 derive instance genericA :: Generic A
+instance showA :: Show A where show = gShow
 
 -- Can't decode types with multiple data constructors:
 data B = B1 { name :: String } | B2
 instance eqB :: Eq B where
   eq _ _ = false
 derive instance genericB :: Generic B
+instance showB :: Show B where show = gShow
 
 -- Handle maybe:
 data MA = MA { name :: Maybe String }
 instance eqMA :: Eq MA where
   eq (MA a) (MA b) = eq a.name b.name
 derive instance genericMA :: Generic MA
-instance showMA :: Show MA where
-  show = gShow
+instance showMA :: Show MA where show = gShow
 
 testJson = fromRight (P.jsonParser "{\"name\": \"name\"}")
 emptyJson = fromRight (P.jsonParser "{}")
 
 
 main = runTest do
-  test "gDecode" do
-    Assert.assert "expect success" $
-      (gDecode testJson == Right (A {name: "name"}) :: Either String A)
-    Assert.assert "expect broken data constructor" $
-      (gDecode testJson == Left "When decoding a Test.Main.B: Must have exactly one data constructor."  :: Either String B)
+  test "gDecode valid case" do
+    Assert.equal (gDecode testJson) (Right (A {name: "name"}))
+  test "gDecode too many constructor branches" do
+    Assert.equal (gDecode testJson) (Left "When decoding a Test.Main.B: Must have exactly one data constructor."  :: Either String B)
   test "gDecode maybe empty JSON should be Nothing" $
     Assert.equal (Right (MA {name: Nothing})) (gDecode emptyJson)
   test "gDecode maybe valid JSON should be Just" $
