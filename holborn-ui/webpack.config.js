5b557cd1b29c1a44487a1829c17b259fe85549a7
Tom Hunger <tehunger@gmail.com>
2016-03-04 23:29:41 +0000

    Add webpack config that splits into vendor and app

---
+++ holborn-ui/webpack.config.js
@@ -0,0 +1,35 @@
+// webpack.config.js
+'use strict';
+var webpack = require("webpack");
+
+var PurescriptWebpackPlugin = require('purescript-webpack-plugin');
+
+var src = ['bower_components/purescript-*/src/**/*.purs', 'src/**/*.purs'];
+
+var ffi = ['bower_components/purescript-*/src/**/*.js'];
+
+var modulesDirectories = [
+  'node_modules',
+  'bower_components'
+];
+
+var config = {
+  entry: {
+    app: './src/Main.purs',
+    vendor: ["react", "react-dom"]
+  },
+  output: { path: __dirname
+            , pathinfo: true
+            , filename: 'bundle.js'
+          },
+  module: { loaders: [ { test: /\.purs$/
+                         , loader: 'purs-loader'
+                       } ] },
+  resolve: { modulesDirectories: modulesDirectories },
+  plugins: [ new PurescriptWebpackPlugin({src: src, ffi: ffi}),
+             new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js"),
+           ]
+};
+
+
+module.exports = config;
