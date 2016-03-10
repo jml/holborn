913329f78719116c17c8b236328b2a6dd887c6e3
Tom Hunger <tehunger@gmail.com>
2016-03-10 16:01:44 +0000

    Use nix to get bower and node modules.

--- holborn-ui/default.nix
+++ holborn-ui/default.nix
@@ -1,12 +1,25 @@
-{ nodejs, stdenv, haskellPackages, purescript-derive-lenses, sassc }:
+{ callPackage, nodejs, stdenv, haskellPackages, sassc }:
+let
+  node_modules = callPackage ../nix/node_modules.nix {};
+  bower_modules = callPackage ../nix/bower_modules.nix { inherit node_modules; };
+in
 stdenv.mkDerivation {
   name = "holborn-ui";
   src = ./.;
-  propagatedBuildInputs = [
+  buildInputs = [
     nodejs
     haskellPackages.purescript
     haskellPackages.psc-ide
     sassc
-    # purescript-derive-lenses  # not working due to build failure.
+    node_modules
+    bower_modules
   ];
+  shellHook = ''
+    rm -rf node_modules bower_components
+
+    # Can't symlink because js developers have never heard of such a
+    # thing.
+    cp -r ${node_modules}/node_modules .
+    cp -r ${bower_modules}/bower_components .
+  '';
 }
