185f16646fb8703d479fa8a40140fabe6c81910b
Tom Hunger <tehunger@gmail.com>
2016-03-12 14:53:39 +0000

    Illustrate how to forward raw requests.

--- holborn-api/lib/Holborn/API/Browse.hs
+++ holborn-api/lib/Holborn/API/Browse.hs
@@ -23,16 +23,24 @@ import Data.Aeson (Value(..), object, (.=))
 import Servant
 
 import Holborn.API.Types (AppConf(..), Username)
-import Holborn.JSON.Browse as B
 import Holborn.Auth (AuthToken(..), Permission(..), hasPermission, getAuthFromToken)
 import Holborn.Errors (JSONCodeableError(..))
-import Network.Wai (Application, requestMethod, rawPathInfo)
-import Network.HTTP.ReverseProxy (waiProxyTo, defaultOnExc, WaiProxyResponse(WPRModifiedRequest), ProxyDest(..))
+import Network.Wai (Application, responseLBS)
+import Network.HTTP.ReverseProxy (waiProxyTo, defaultOnExc, WaiProxyResponse(WPRModifiedRequest, WPRResponse), ProxyDest(..))
 import Network.HTTP.Client (newManager, defaultManagerSettings)
+import Network.HTTP.Types.Status (status404)
 
+type Owner = Text
+type Repo = Text
 
 type API =
-    "v1" :> Header "Authorization" AuthToken :> "browse" :> ReqBody '[JSON] B.BrowseRequest :> Raw
+    "v1"
+    :> "repos"
+    :> Header "Authorization" AuthToken
+    :> Capture "owner" Owner
+    :> Capture "repo" Repo
+    :> Raw
+
 
 data BrowseError = NotFound
 
@@ -44,13 +52,12 @@ server :: AppConf -> Server API
 server conf = browse conf
 
 
-browse :: AppConf -> Maybe AuthToken -> B.BrowseRequest -> Application
-browse AppConf{conn, httpManager} token B.BrowseRequest{..} =
+browse :: AppConf -> Maybe AuthToken -> Owner -> Repo -> Application
+browse AppConf{conn, httpManager} token owner repo =
     waiProxyTo proxy defaultOnExc httpManager
   where
-    -- This is a hack to map repositories to the current REPO
-    -- backend. Eventually we'll just clean & then pass through the
-    -- API request (can't trust internet inputs).
+    -- pas the raw request through if the user is authorized
     proxy request = do
-        return $ let modifiedRequest = request { requestMethod = "GET", rawPathInfo = encodeUtf8 _BrowseRequest_repo }
-                 in  WPRModifiedRequest modifiedRequest (ProxyDest "127.0.0.1" 8080)
+        return $ case owner of
+            "jml" -> WPRResponse (responseLBS status404 [] "not found")
+            _  -> WPRModifiedRequest request (ProxyDest "127.0.0.1" 8080)
