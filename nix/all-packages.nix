096394f28d2660212c5a0ce0e39cef152ff490ee
Tom Hunger <tehunger@gmail.com>
2016-03-14 17:42:10 +0000

    Fx _o_split compile bug. See comment.

--- nix/all-packages.nix
+++ nix/all-packages.nix
@@ -3,6 +3,18 @@
 { haskellPackages, haskell }:
 haskellPackages.override {
     overrides = self: super: {
+
+      # We're using `src = ./.` as the source attribute which
+      # unfortunately includes everything in that directory including
+      # ./dist which can lead to errors like "_o_split_User not
+      # found". The best solution would be to filter the source
+      # accordingly with builtins.filterSource but I could quite make
+      # that work and still keep the "cabal2nix . > default.nix"
+      # workflow. 2nd best solution is to delete ./dist before
+      # compiling.
+      mkDerivation = { pname, ... }@args:
+        super.mkDerivation (if builtins.substring 0 7 pname == "holborn" then (args // { preCompileBuildDriver = "rm -rf ./dist";}) else args);
+
       hcl = self.callPackage ../hcl {};
       holborn-api = self.callPackage ../holborn-api {};
       holborn-repo = self.callPackage ../holborn-repo {};
