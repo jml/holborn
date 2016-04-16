c8f731d2253349e03e6fc117ffe5dbbc1b45a74e
Jonathan Lange <jml@mumak.net>
2016-04-16 18:56:39 +0100

    Remove unused dependencies

--- holborn-api/default.nix
+++ holborn-api/default.nix
@@ -1,10 +1,9 @@
 { mkDerivation, aeson, attoparsec, base, basic-prelude, blaze-html
-, bytestring, containers, either, entropy, envparse, errors
-, holborn-common-types, http-api-data, http-client
-, http-reverse-proxy, http-types, jwt, postgresql-simple, process
-, QuickCheck, servant, servant-blaze, servant-docs, servant-server
-, shakespeare, stdenv, tasty, tasty-hunit, tasty-quickcheck, text
-, time, transformers, wai, warp
+, blaze-markup, bytestring, either, envparse, errors
+, holborn-common-types, http-client, http-reverse-proxy, http-types
+, jwt, postgresql-simple, QuickCheck, servant-docs, servant-server
+, stdenv, tasty, tasty-hunit, tasty-quickcheck, text, time
+, transformers, wai, warp
 }:
 mkDerivation {
   pname = "holborn-api";
@@ -13,16 +12,14 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson attoparsec base basic-prelude blaze-html bytestring
-    containers either entropy errors holborn-common-types http-api-data
-    http-client http-reverse-proxy http-types jwt postgresql-simple
-    process servant servant-blaze servant-docs servant-server
-    shakespeare text time transformers wai
+    aeson attoparsec base basic-prelude blaze-html blaze-markup
+    bytestring either errors holborn-common-types http-client
+    http-reverse-proxy http-types jwt postgresql-simple servant-docs
+    servant-server text time transformers wai
   ];
   executableHaskellDepends = [
-    base basic-prelude blaze-html bytestring envparse
-    holborn-common-types http-client postgresql-simple servant-server
-    wai warp
+    base basic-prelude envparse holborn-common-types http-client
+    postgresql-simple servant-server wai warp
   ];
   testHaskellDepends = [
     base basic-prelude errors holborn-common-types postgresql-simple
--- holborn-common-types/default.nix
+++ holborn-common-types/default.nix
@@ -1,15 +1,14 @@
 { mkDerivation, aeson, base, basic-prelude, bcrypt, bytestring
 , containers, entropy, http-api-data, http-client
-, postgresql-simple, process, stdenv, time, transformers
+, postgresql-simple, process, stdenv, time
 }:
 mkDerivation {
   pname = "holborn-common-types";
-  version = "0.1.0.0";
+  version = "0.1.3.0";
   src = ./.;
   libraryHaskellDepends = [
     aeson base basic-prelude bcrypt bytestring containers entropy
     http-api-data http-client postgresql-simple process time
-    transformers
   ];
   license = stdenv.lib.licenses.unfree;
 }
