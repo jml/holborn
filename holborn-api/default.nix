f3a735c75bb6e6860078f590c8aa666e96980e3d
Tom Hunger <tehunger@gmail.com>
2016-05-25 21:03:15 +0100

    Sketch how to do hspec-wai.

--- holborn-api/default.nix
+++ holborn-api/default.nix
@@ -1,9 +1,10 @@
 { mkDerivation, aeson, attoparsec, base, basic-prelude, blaze-html
 , blaze-markup, bytestring, either, envparse, errors
-, holborn-common-types, http-client, http-reverse-proxy, http-types
-, jwt, postgresql-simple, QuickCheck, servant-docs, servant-server
-, stdenv, tasty, tasty-hunit, tasty-quickcheck, text, time
-, transformers, wai, wai-cors, warp
+, holborn-common-types, hspec-wai, http-client, http-reverse-proxy
+, http-types, jwt, postgresql-simple, process, QuickCheck
+, servant-docs, servant-server, stdenv, tasty, tasty-hspec
+, tasty-hunit, tasty-quickcheck, text, time, transformers, wai
+, wai-cors, warp
 }:
 mkDerivation {
   pname = "holborn-api";
@@ -15,15 +16,16 @@ mkDerivation {
     aeson attoparsec base basic-prelude blaze-html blaze-markup
     bytestring either errors holborn-common-types http-client
     http-reverse-proxy http-types jwt postgresql-simple servant-docs
-    servant-server text time transformers wai
+    servant-server text time transformers wai warp
   ];
   executableHaskellDepends = [
-    base basic-prelude envparse holborn-common-types http-client
-    postgresql-simple servant-server wai wai-cors warp
+    base basic-prelude envparse holborn-common-types servant-server wai
+    wai-cors warp
   ];
   testHaskellDepends = [
-    base basic-prelude errors holborn-common-types postgresql-simple
-    QuickCheck tasty tasty-hunit tasty-quickcheck
+    aeson base basic-prelude bytestring errors holborn-common-types
+    hspec-wai process QuickCheck tasty tasty-hspec tasty-hunit
+    tasty-quickcheck
   ];
   license = stdenv.lib.licenses.unfree;
 }
--- holborn-api/tests/Tests.hs
+++ holborn-api/tests/Tests.hs
@@ -6,7 +6,7 @@ import BasicPrelude
 
 import Data.Aeson (FromJSON, ToJSON, decode)
 import Data.ByteString.Lazy (fromStrict)
-import Test.Tasty (defaultMain, TestTree, testGroup)
+import Test.Tasty (defaultMain, TestTree, testGroup, withResource)
 import Test.Tasty.HUnit hiding (assert)
 import Test.Tasty.QuickCheck
 import Holborn.API.Types (newPassword)
@@ -15,10 +15,52 @@ import Holborn.API.SSH (shellEncode)
 import Holborn.JSON.SSHRepoCommunication (RepoCall)
 import System.Process (shell, readCreateProcess)
 import Test.QuickCheck.Monadic (PropertyM, assert, monadicIO, monitor, run)
+import Test.Tasty.Hspec (testSpec, describe, it)
+import Test.Hspec.Wai (with, get, shouldRespondWith)
+
+import Holborn.API.Config (AppConf, Config(..), loadAppConf)
+import Network.Wai (Application)
+import Holborn.API (api, server)
+import Servant (serve)
+import System.Process (callCommand)
 
 
 main :: IO ()
-main = defaultMain tests
+main = do
+    waiTests <- waiTest
+    defaultMain (testGroup "all" [waiTests, tests])
+
+
+testApp :: IO Application
+testApp = do
+    callCommand "createuser holborn-test-user || true"
+    callCommand "dropdb holborn-test-db || true"
+    callCommand "createdb -O holborn-test-user holborn-test-db"
+    let conf = Config
+          { port = 9999
+          , pgDb = "holborn-test-db"
+          , pgUser = "holborn-test-user"
+          , pgPort = 5432
+          , configBaseUrl = "http://127.0.0.1:9999/"
+          , configStaticBaseUrl = ""
+          , configRepoHostname = ""
+          , configRepoPort = 0
+          , configRawRepoHostname = ""
+          , configRawRepoPort = 0
+          }
+    print "Using config:"
+    print conf
+    appConf <- loadAppConf conf
+
+    pure (serve api (server appConf))
+
+
+waiTest :: IO TestTree
+waiTest =
+  testSpec "wai-tests" $ with (testApp) $ do
+    describe "GET /" $ do
+        it "responds with 200" $ do
+            get "/" `shouldRespondWith` 200
 
 
 stringToBytes :: String -> LByteString
