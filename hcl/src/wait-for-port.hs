c6e3480dcdbb49cdc731094364278226dfeb846b
Jonathan Lange <jml@mumak.net>
2016-06-24 19:22:01 +0100

    Implement exponential backoff for wait-for-port

--- hcl/src/wait-for-port.hs
+++ hcl/src/wait-for-port.hs
@@ -3,7 +3,7 @@ module Main (main) where
 
 import HolbornPrelude
 
-import Turtle hiding (option, options)
+import Turtle hiding (hostname, option, options)
 import Control.Concurrent (threadDelay)
 import Network.Simple.TCP (connectSock)
 import Options.Applicative
@@ -20,37 +20,50 @@ import Options.Applicative
   , option
   , progDesc
   , short
+  , str
+  , value
   )
 import System.Exit (exitWith)
 
 
-options :: ParserInfo (Int, Int)
+data Config = Config { port :: Int
+                     , timeout :: Int
+                     , hostname :: String }
+
+
+options :: ParserInfo Config
 options = info (helper <*> parser) description
   where
-    parser = (,) <$> option auto ( long "port" <> metavar "PORT" <> help "port to check" )
-                 <*> option auto ( long "timeout" <> short 't' <> metavar "TIMEOUT"
-                                   <> help "seconcds to wait before failing" )
+    parser = Config <$> option auto ( long "port" <> metavar "PORT" <> help "port to check" )
+                    <*> option auto ( long "timeout" <> short 't' <> metavar "TIMEOUT"
+                                      <> help "seconcds to wait before failing" )
+                    <*> option str ( long "host" <> metavar "HOST" <> help "host to check"
+                                     <> value "127.0.0.1" )
     description = concat
       [ fullDesc
-      , progDesc "Wait for --timeout seconds for a port to become active, then fail if it didn't. Retries once a second."
+      , progDesc "Wait for --timeout seconds for a port to become active, then fail if it didn't. Retries in 50ms then backs off exponentially."
       , header "wait-for-port - wait for a port to become active"
       ]
 
+-- | Repeatedly poll host:port until we connect or we reach a timeout.
+--
+-- Delay and timeout are both in milliseconds.
+waitForPort :: String -> Int -> Int -> Int -> IO ExitCode
+waitForPort host port timeout delay
+  | timeout <= 0 = do
+      print ("Could not connect to " <> fromString host <> ":" <> show port)
+      pure (ExitFailure 1)
+  | otherwise = do
+      result <- try (connectSock host ((textToString . show) port))
+      case result of
+        Left (_ :: IOException) -> do
+          threadDelay (delay * 1000)
+          waitForPort host port (timeout - delay) (delay * 2)
+        Right _ -> return ExitSuccess
+
+
 main :: IO ()
 main = do
-    (port, timeout) <- execParser options
-    check port timeout >>= exitWith
-
-  where
-    check :: Int -> Int -> IO ExitCode
-    check port timeout
-      | timeout <= 0 = do
-            print ("Could not connect to " :: String, port)
-            return (ExitFailure 1)
-      | otherwise = do
-            result <- try (connectSock "127.0.0.1" ((textToString . show) port))
-            case result of
-             Left (_ :: IOException) -> do
-                 threadDelay (1000 * 1000)
-                 check port (timeout - 1)
-             Right _ -> return ExitSuccess
+    Config{port, timeout, hostname} <- execParser options
+    exitCode <- waitForPort hostname port (timeout * 1000) 50
+    exitWith exitCode
