e3187bc17c82cb89e8dfcb4e6edbed1e4c9cb5b8
Tom Hunger <tehunger@gmail.com>
2016-08-02 13:55:52 +0100

    Cleanup imports for main entry point + remove wrong comment.

--- holborn-ui/src/Main.purs
+++ holborn-ui/src/Main.purs
@@ -6,25 +6,20 @@ module Main (main) where
 
 import Prelude (Unit, (<$>), (<<<), bind, (>>=), void, pure, unit)
 
-import Data.Nullable (toMaybe)
-
-import Control.Monad.Eff (Eff)
-
 import Components.Router as Router
-
-import React as R
-import ReactDOM as RD
+import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE())
-
 import DOM (DOM) as DOM
 import DOM.HTML (window) as DOM
 import DOM.HTML.Types (htmlDocumentToParentNode) as DOM
 import DOM.HTML.Window (document) as DOM
 import DOM.Node.ParentNode (querySelector) as DOM
 import Data.Maybe (Maybe(..))
+import Data.Nullable (toMaybe)
+import React as R
+import ReactDOM as RD
 
 
--- | The main method creates the task list component, and renders it to the document body.
 main :: forall eff. Eff (dom :: DOM.DOM, console :: CONSOLE | eff) Unit
 main = do
   let reactElement = (R.createFactory Router.component {})
