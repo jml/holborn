48ba7ca53d7fcc2286136db87e10aa5c6a446729
Tom Hunger <tehunger@gmail.com>
2016-08-02 13:54:37 +0100

    Update invalid comment.

--- holborn-ui/src/Main.purs
+++ holborn-ui/src/Main.purs
@@ -1,15 +1,6 @@
--- | This task list application is made up of two Thermite components:
--- |
--- | - a component for a single task (Components.Task)
--- | - a component for a list of tasks (Components.TaskList)
--- |
--- | This example demonstrates the main features of a Thermite app:
--- |
--- | - Using `simpleSpec` to create simple components from `Render` and
--- |   `PerformAction` functions
--- | - Composing components using the `Monoid` instance and lens combinators.
--- |
--- | For each component we start by declaring action and state types.
+-- | holborn is a single-page app (SPA) and this is the main entry
+-- point. We are loaded in a html page which contains exactly one div
+-- with id `container` and place the app in that div.
 
 module Main (main) where
 
