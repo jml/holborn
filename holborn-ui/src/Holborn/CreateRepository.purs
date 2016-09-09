ed68f530166bf30541a1f2be36248c1369b8b1c8
Tom Hunger <tehunger@gmail.com>
2016-09-09 16:31:16 +0100

    Some warnings. _a

---
+++ holborn-ui/src/Holborn/CreateRepository.purs
@@ -0,0 +1,76 @@
+module Holborn.CreateRepository where
+
+import Prelude
+
+import Control.Alt ((<|>))
+import Control.Apply ((<*))
+import Control.Monad.Aff (attempt)
+import Control.Monad.Eff.Exception as E
+import Control.Monad.Trans (lift)
+import Data.Argonaut.Encode (encodeJson)
+import Data.Maybe (Maybe(..))
+import Network.HTTP.Affjax as AJ
+import React.DOM as R
+import React.DOM.Props as RP
+import Standalone.Router.Dispatch (Navigate, navigateA)
+import Thermite as T
+import Unsafe.Coerce (unsafeCoerce)
+
+import Holborn.Auth as Auth
+import Holborn.Config (makeUrl)
+import Holborn.Forms as HF
+import Holborn.ManualEncoding.CreateAccount (CreateAccountData(..), CreateAccountDataError(..), username)
+
+import Debug.Trace
+
+type State =
+    { formErrors :: CreateAccountDataError
+    , formData :: CreateAccountData
+    , loading :: Boolean
+    , errors :: Array String
+    }
+
+
+flashError :: forall a. String -> ({ errors :: Array String | a } -> { errors :: Array String | a })
+flashError msg = \s -> s { errors = (s.errors <> [msg]) }
+
+
+initialState :: State
+initialState =
+  { formErrors: CreateAccountDataError {username: Nothing}
+  , formData: CreateAccountData {username: ""}
+  , loading: false
+  , errors: []
+  }
+
+data Action = CreateRepo | UpdateFormData CreateAccountData
+
+
+spec :: forall eff props. T.Spec (err :: E.EXCEPTION, ajax :: AJ.AJAX, navigate :: Navigate | eff) State props Action
+spec = T.simpleSpec performAction render
+  where
+    render :: T.Render State props Action
+    render dispatch props ({ formData, formErrors: CreateAccountDataError err, loading, errors }) _ =
+      [ R.h1 [] [R.text "Create a new repository"]
+      , R.form [RP.onSubmit onSubmit]
+        [ HF.text "http://code.space/me/" err.username username (dispatch <<< UpdateFormData) formData
+        , R.button [RP.disabled loading, RP.className "btn btn-default"] [R.text if loading then "Finishing ..." else "Finish setup"]
+        ]
+      , R.ul [] (map (\x -> R.li [] [R.text x]) errors)
+      ]
+      where
+        onSubmit ev = do
+          (unsafeCoerce ev).preventDefault
+          dispatch CreateRepo
+
+    performAction :: forall eff'. T.PerformAction (err :: E.EXCEPTION, ajax :: AJ.AJAX, navigate :: Navigate | eff') State props Action
+    performAction CreateRepo props state = do
+      T.cotransform (\state -> state { loading = true })
+      r <- lift $ attempt (Auth.post (makeUrl "/v1/create-repository") (encodeJson state.formData))
+      case Auth.handleResult r of
+        Auth.OK (_ :: Unit) -> lift (navigateA "/")
+        Auth.FormError errs -> void $ T.cotransform $ \state -> state { formErrors = errs }
+        Auth.OtherError _ -> void $ T.cotransform $ id
+      void $ T.cotransform (\state -> state { loading = false })
+
+    performAction (UpdateFormData x) _ state = void $ T.cotransform $ \state -> state { formData = x }
